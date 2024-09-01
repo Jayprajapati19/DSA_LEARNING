f9611c22c615ea01511b365961add4f641becd75
JAY PRAJAPATI <jayp37679@gmail.com>
2024-09-01 10:41:06 +0530

    solved pattern quetions

--- basic/pattern3.cpp
+++ basic/pattern3.cpp
@@ -7,14 +7,15 @@ int main()
    cin >> n;
 
    int i = 1;
+   int count = 1;
 
    while (i <= n)
    {
       int j = 1;
       while (j <= n)
       {
-         // cout << j;
-         cout << n - j + 1;
+         cout << count << " ";
+         count = count + 1;
          j = j + 1;
       }
       cout << endl;
