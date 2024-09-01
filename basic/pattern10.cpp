4ee57d7ed1ac4b1c58e351b01a2f7907899b4a93
JAY PRAJAPATI <jayp37679@gmail.com>
2024-09-01 19:31:28 +0530

    pattern

--- basic/pattern10.cpp
+++ basic/pattern10.cpp
@@ -12,9 +12,12 @@ int main()
    while (row <= n)
    {
       int col = 1;
+      // int value = row;
+
       while (col <= row)
       {
          cout << row + col - 1;
+         // value = value + 1;
          col = col + 1;
       }
       cout << endl;
