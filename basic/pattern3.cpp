b23eb1042fbcc5a80c82b6fea332f62d6976ec24
JAY PRAJAPATI <jayp37679@gmail.com>
2024-09-01 12:02:55 +0530

    3 pattern quetions

--- basic/pattern3.cpp
+++ basic/pattern3.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main()
 {
    int n;
+
    cin >> n;
 
    int i = 1;
    int count = 1;
 
    while (i <= n)
+
    {
       int j = 1;
       while (j <= n)
@@ -18,6 +20,7 @@ int main()
          count = count + 1;
          j = j + 1;
       }
+
       cout << endl;
       i = i + 1;
    }
