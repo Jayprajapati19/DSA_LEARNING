680990d48e6f4bdfad2e923d4b1ae893369d749e
JAY PRAJAPATI <jayp37679@gmail.com>
2024-09-06 00:42:01 +0530

    reverse integer

---
+++ binary-decimal/reverseInteger.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <math.h>
+#include <bitset>
+
+using namespace std;
+
+int main()
+
+{
+   /*
+   int n;
+   cin >> n;
+
+   int answer = 0;
+   int i = 0;
+
+   while (n != 0)
+   {
+      int bit = n & 1;
+
+      answer = (bit * pow(10, i) + answer);
+      n = n >> 1;
+      i++;
+   }
+
+   cout << "Answer Is " << answer << endl;
+}
+
+
+
+1. n = -6 --> -ve ignore
+2. n = 6 -> 2's Comp 1's -> + 1
+ */
+
+   // int n;
+   // cout << "Enter a number: ";
+   // cin >> n;
+
+   // // Handle negative numbers by using bitset (assuming 8 bits for simplicity)
+   // // You can increase to 16, 32, or 64 depending on the system.
+   // cout << "Binary representation: " << bitset<8>(n) << endl;
+
+   // right shift se divide /
+   // left shift se multiple.... *
+
+   // d --> b , b --> d;
+}
