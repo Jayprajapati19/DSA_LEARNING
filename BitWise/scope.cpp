66b35dbac184242efee1aa395f6f73c0f6d208e8
JAY PRAJAPATI <jayp37679@gmail.com>
2024-09-05 23:14:50 +0530

    scope

--- BitWise/scope.cpp
+++ BitWise/scope.cpp
@@ -9,6 +9,19 @@ int main()
 
    if (true)
    {
-      cout << a << endl;
+      int b = 5;
+      cout << b << endl;
+   }
+
+   cout << a << endl;
+   int b = 1;
+   cout << b << endl;
+
+   int c = 3;
+   cout << b << endl;
+
+   for (int i = 0; i < 8; i++)
+   {
+      cout << "HII" << endl;
    }
 }
---
+++ BitWise/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+   // int a = 9;
+   // cout << a << endl;
+
+   // if (true)
+   // {
+   //    int b = 5;
+   //    cout << b << endl;
+   // }
+
+   // cout << a << endl;
+   // int b = 1;
+   // cout << b << endl;
+
+   // int c = 3;
+   // cout << b << endl;
