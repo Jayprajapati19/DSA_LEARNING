3e1b8d14a2af3e8441ef640088749f8a13ac5e7e
Jayprajapati19 <jayp37679@gmail.com>
2024-09-23 18:16:21 +0530

    bubble sort

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -8,12 +8,19 @@ void bubbleSort(int arr[], int n)
     {
         // for round 1 to and n-1
 
-        for (int j = 0; j < n - 1; j++)
+        bool swapped = false;
+
+        for (int j = 0; j < n - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
+        }
+        if (swapped == false)
+        {
+            break;
         }
     }
 }
