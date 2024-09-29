5967791affb782bcb9bdae585be20af0f5be4d24
Jayprajapati19 <jayp37679@gmail.com>
2024-09-29 13:01:00 +0530

    creation of 2d Array

---
+++ lecture_23/twoD_arrayIntro.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // creation of 2D Array
+
+    int arr[3][4];
+
+    /*
+
+    // input from user -> row wise input
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    */
+
+    // input from user -> col wise input
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[j][i];
+        }
+    }
+
+    // print results
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
