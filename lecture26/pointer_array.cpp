ba0891c21f2c50c3693c447a88c8137b265a12b2
Jayprajapati19 <jayp37679@gmail.com>
2024-10-01 22:34:42 +0530

    pointer

--- lecture26/pointer_array.cpp
+++ lecture26/pointer_array.cpp
@@ -4,11 +4,26 @@ using namespace std;
 
 int main()
 {
-    int arr[10] = {5, 9, 19, 3};
+    int arr[10] = {22, 122, 41, 67};
 
+    /*
     cout << "First Memory Block is " << arr << endl;
     cout << arr[0] << endl;
     cout << "First Memory Block is " << &arr[0] << endl;
 
     cout << "4th " << *arr << endl;
+    cout << "5th " << *arr + 1 << endl;
+    cout << "6th " << *(arr + 1) << endl;
+    cout << "7th " << *(arr) + 1 << endl;
+    cout << "8th " << arr[2] << endl;
+
+    int i = 3;
+    cout << i[arr] << endl;
+    */
+
+    int temp[10];
+    cout << sizeof(temp) << endl;
+
+    int *ptr = &temp[0];
+    cout << sizeof(ptr) << endl;
 }
