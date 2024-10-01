94416def446d752a8b2c52040327dadcb4730e81
Jayprajapati19 <jayp37679@gmail.com>
2024-10-01 23:31:27 +0530

    char array pointer

--- lecture26/pointer_array.cpp
+++ lecture26/pointer_array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int arr[10] = {22, 122, 41, 67};
+    // int arr[10] = {22, 122, 41, 67};
 
     /*
     cout << "First Memory Block is " << arr << endl;
@@ -19,11 +19,40 @@ int main()
 
     int i = 3;
     cout << i[arr] << endl;
-    */
 
-    int temp[10];
+    int temp[10] = {1, 2, 3};
     cout << sizeof(temp) << endl;
+    cout << "1st " << sizeof(*temp) << endl;
+    cout << "2st " << sizeof(&temp) << endl;
+    cout << "3rd " << sizeof(&temp) + 1 << endl;
+    cout << "4th " << sizeof(*temp) + 1 << endl;
 
     int *ptr = &temp[0];
     cout << sizeof(ptr) << endl;
+    cout << sizeof(*ptr) << endl;
+    cout << sizeof(&ptr) << endl;
+
+
+    int a[20] = {1, 2, 4, 5};
+    cout << "-> " << &a[0] << endl;
+    cout << &a << endl;
+    cout << a << endl;
+
+    int *p = &a[0];
+    cout << &p[0] << endl;
+    cout << &p << endl;
+    cout << "-> " << p << endl;
+*/
+
+    int arr[10];
+
+    //    ERROR: arr = arr + 1;
+
+    int *ptr = &arr[0];
+    cout << ptr << endl;
+
+    ptr = ptr + 1;
+    cout << ptr << endl;
+
+    return 0;
 }
---
+++ lecture26/pointer_char_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    char ch[5] = "jpdi";
+
+    cout << arr << endl;
+    cout << ch << endl;
+
+    char *c = &ch[0];
+    // priting intire string
+    cout << c << endl;
+}
