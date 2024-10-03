92d00fde6300b93c33525d9fd41a3ef621270765
Jayprajapati19 <jayp37679@gmail.com>
2024-10-03 11:28:17 +0530

    double pointer praqctice

--- lecture27/double_pointer.cpp
+++ lecture27/double_pointer.cpp
@@ -4,16 +4,24 @@ using namespace std;
 
 int main()
 {
-    // int i = 5;
-    // int *p = &i;
-    // int **p2 = &p;
+    /*
+     */
 
-    // cout << "Print p" << p << endl;
-    // // cout << "Address p" << &p << endl;
+    int i = 5;
+    int *p = &i;
+    int **p2 = &p;
+
+    cout << "Print p" << p << endl;
+    // cout << "Address p" << &p << endl;
     // cout << *p2 << endl;
 
-    // cout << i << endl;
-    // cout << *p << endl;
-}
+    cout << i << endl;
+    cout << *p << endl;
 
-// 16min lec-27
+    cout << &i << endl;
+    cout << p << endl;
+    cout << *p2 << endl;
+
+    cout << &p << endl;
+    cout << p2 << endl;
+}
