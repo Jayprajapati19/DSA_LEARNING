3895345cf6515c30544ac061b9f86724aa5f7a04
Jayprajapati19 <jayp37679@gmail.com>
2024-10-03 12:01:25 +0530

    mcq solved

--- lecture27/double_pointer.cpp
+++ lecture27/double_pointer.cpp
@@ -2,12 +2,26 @@
 
 using namespace std;
 
+void update(int **p)
+{
+    // p = p + 1;
+    // change hota - NO ?
+
+    // *p = *p + 1;
+    // change hota - YES ?
+
+    **p = **p + 1;
+    // change hota - YES?
+}
+
 int main()
 {
     /*
-     */
 
     int i = 5;
+    int *p = &i;
+    int **p2 = &p;
+
     int *p = &i;
     int **p2 = &p;
 
@@ -24,4 +38,26 @@ int main()
 
     cout << &p << endl;
     cout << p2 << endl;
+
+    cout << endl
+         << endl;
+    cout << "before " << i << endl;
+    cout << "before " << p << endl;
+    cout << "before " << p2 << endl;
+    update(p2);
+    cout << "after " << i << endl;
+    cout << "after " << p << endl;
+    cout << "after " << p2 << endl;
+
+    cout << endl
+         << endl;
+     */
+
+    int *p = 0;
+    int first = 110;
+    // *p = first;
+    *p = first;
+    cout << *p << endl;
+
+    // Pointer ka size hamesh 8 honga
 }
