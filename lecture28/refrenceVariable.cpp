cd594c323a681d233673590858be46a2e959189f
Jayprajapati19 <jayp37679@gmail.com>
2024-10-04 22:20:04 +0530

    refrence variable

--- lecture28/refrenceVariable.cpp
+++ lecture28/refrenceVariable.cpp
@@ -2,8 +2,35 @@
 
 using namespace std;
 
+// int &func(int a)
+// {
+//     int num = a;
+//     int &ans = num;
+//     return ans;
+// }
+
+int *func(int n)
+{
+    int *ptr = &n;
+    return ptr;
+}
+
+// reference variable me value incremente hongi --> pass by reference nayi memory create nahi hoti
+void update2(int &n)
+{
+    n++;
+}
+
+// pass by value me nayi memory create  hotib hai --> variable copy ho jata hai
+void update1(int n)
+{
+    n++;
+}
+
 int main()
 {
+
+    /*
     int i = 5;
 
     // ref varible
@@ -14,6 +41,12 @@ int main()
     cout << i << endl;
     j++;
     cout << i << endl;
+    */
 
-    // 5min
+    int n = 5;
+    cout << "Before " << n << endl;
+    update2(n);
+    cout << "After  " << n << endl;
+
+    func(n);
 }
