0437e52d7e9a915bc02befa85e63a46dea9ae9e5
Jayprajapati19 <jayp37679@gmail.com>
2024-10-07 01:05:18 +0530

    default args globl var inline functions

---
+++ lecture_30/global_variable.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void a(int &i)
+{
+    cout << i << endl;
+    b(i);
+}
+
+void b(int &i)
+{
+    cout << i << endl;
+}
+
+int main()
+{
+    int i = 5;
+    a(i);
+}
