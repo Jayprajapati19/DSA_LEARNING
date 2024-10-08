9ba74e4ba3d00b0208e47b4db8b86fe9debccadc
Jayprajapati19 <jayp37679@gmail.com>
2024-10-08 10:22:04 +0530

    recursion

---
+++ lec_32_Recursion/practice.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void reachHome(int src, int dest)
+{
+    // base case
+
+    if (src == dest)
+    {
+        cout << "pahuch gaya" << endl;
+        return;
+    }
+
+    // Processing - ak step age badh jaoo..
+    src++;
+
+    // recursive call
+    reachHome(src, dest);
+}
+
+int main()
+{
+    int dest = 10;
+    int src = 1;
+
+    cout << endl;
+    reachHome(src, dest);
+}
