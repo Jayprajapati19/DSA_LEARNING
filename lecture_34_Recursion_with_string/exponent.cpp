e965e71eb44bf225aedab9fb06c9214d5cebdfb6
Jayprajapati19 <jayp37679@gmail.com>
2024-10-21 02:07:58 +0530

    merge sort lec 35

--- lecture_34_Recursion_with_string/exponent.cpp
+++ lecture_34_Recursion_with_string/exponent.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// There is a lot to learn, Keep in mind “ Mnn bhot karega k chor yrr apne se nahi yoga ya maza nahi aara, Just ask 1 question “ Why I started ?
+
 using namespace std;
 
 int power(int a, int b)
