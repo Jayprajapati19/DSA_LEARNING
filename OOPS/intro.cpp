d1fd69fc840907c413669a3f109457e719a272f2
Jayprajapati19 <jayp37679@gmail.com>
2024-10-27 22:40:12 +0530

    learned about constructor

--- OOPS/intro.cpp
+++ OOPS/intro.cpp
@@ -12,6 +12,11 @@ private:
 public:
     char level;
 
+    Hero()
+    {
+        cout << "Constructor Called" << endl;
+    }
+
     void print()
     {
         cout << level << endl;
@@ -40,15 +45,50 @@ public:
 
 int main()
 {
+
+    // Object created statically
+
+    Hero jayy;
+
+        /*
+
     // creation of object from hero class
     Hero jay;
-    jay.health = 19;
+
+    cout << "size of jay: " << sizeof(jay) << endl;
+
+    cout << "Jay Health is " << jay.gethealth() << endl;
+    // jay.health = 19;
+
+    // use setter
+    jay.sethealth(19);
     jay.level = 'D';
 
-    // cout << "size: " << sizeof(h1) << endl;
-
-    cout << "Health is: " << jay.health << endl;
+    cout << "Health is: " << jay.gethealth() << endl;
     cout << "Level is: " << jay.level << endl;
+
+    */
+
+    /*
+    // static allocation
+    Hero a;
+
+    cout << "Level is: " << a.level << endl;
+    cout << "Health is: " << a.gethealth() << endl;
+
+    // dynamically
+
+    Hero *b = new Hero;
+    b->setlevel('D');
+    b->sethealth(70);
+
+    cout << "Level is: " << (*b).level << endl;
+    cout << "Health is: " << (*b).gethealth() << endl;
+
+    cout << "Level is: " << b->level << endl;
+    cout << "Health is: " << b->gethealth() << endl;
+
+    */
 
     return 0;
 }
