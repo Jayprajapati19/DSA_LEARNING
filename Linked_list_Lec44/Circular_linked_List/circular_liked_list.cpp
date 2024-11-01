50585f11dc129375a85bc7001a23a3017dfd5fa9
Jayprajapati19 <jayp37679@gmail.com>
2024-11-01 13:13:02 +0530

    delete in circular linked list first last middle

--- Linked_list_Lec44/Circular_linked_List/circular_liked_list.cpp
+++ Linked_list_Lec44/Circular_linked_List/circular_liked_list.cpp
@@ -63,6 +63,13 @@ void print(Node *tail)
 {
     Node *temp = tail;
 
+    // empty list
+    if (tail == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
     do
     {
         cout << tail->data << " ";
@@ -71,6 +78,45 @@ void print(Node *tail)
     } while (tail != temp);
     cout << endl;
 };
+
+void deleteNode(Node *&tail, int value)
+{
+    // empty list
+    if (tail == NULL)
+    {
+        cout << "List is empty! Please check again" << endl;
+        return;
+    }
+    else
+    {
+        // non empty list
+        // assuming that "value" is preseting in the Linked list
+        Node *prev = tail;
+        Node *curr = prev->next;
+
+        while (curr->data != value)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+        prev->next = curr->next;
+
+        // 1 node Linked list
+        if (curr == prev)
+        {
+            tail = NULL;
+        };
+
+        // >=2 Node Linked List
+        if (tail == curr)
+        {
+            tail = prev;
+        }
+
+        curr->next = NULL;
+        delete curr;
+    }
+}
 
 int main()
 {
@@ -82,6 +128,40 @@ int main()
     print(tail);
 
     InsertNode(tail, 3, 5);
+    print(tail);
+
+    /*
+
+    InsertNode(tail, 5, 7);
+    print(tail);
+
+    InsertNode(tail, 7, 9);
+    print(tail);
+
+    InsertNode(tail, 9, 19);
+    print(tail);
+
+    InsertNode(tail, 19, 14);
+    print(tail);
+
+    InsertNode(tail, 9, 5);
+    print(tail);
+
+    InsertNode(tail, 19, 99);
+    print(tail);
+
+    // delete at first
+    deleteNode(tail, 3);
+    print(tail);
+
+    // delete at middle
+    deleteNode(tail, 9);
+    print(tail);
+
+    */
+
+    // delete at last
+    deleteNode(tail, 3);
     print(tail);
 
     return 0;
