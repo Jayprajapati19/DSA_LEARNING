9618fe5b41bc05b017b0192161e74f9c07aa13f0
Jayprajapati19 <jayp37679@gmail.com>
2024-11-07 10:19:09 +0530

    add

--- lecture45/reverse_linked_list.cpp
+++ lecture45/reverse_linked_list.cpp
@@ -2,7 +2,67 @@
 
 using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node *reverse_linkedList(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+
+    Node *prev = NULL;
+    Node *curr = head;
+    Node *forward = NULL;
+
+    while (curr != NULL)
+    {
+        forward = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    return prev;
+}
+
+void print_linkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
 int main()
 {
+    // Creating a linked list with nodes 1 -> 2 -> 3 -> 4 -> 5
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    cout << "Original Linked List: ";
+    print_linkedList(head);
+
+    // Reversing the linked list
+    head = reverse_linkedList(head);
+
+    cout << "Reversed Linked List: ";
+    print_linkedList(head);
+
     return 0;
 }
