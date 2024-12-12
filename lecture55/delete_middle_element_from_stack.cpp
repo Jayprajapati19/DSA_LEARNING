2416dae8209ba8bed1c49cb0d82fea47cbab5c0b
Jayprajapati19 <jayp37679@gmail.com>
2024-12-12 09:53:45 +0530

    delete middle in stack

--- lecture55/delete_middle_element_from_stack.cpp
+++ lecture55/delete_middle_element_from_stack.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
@@ -10,15 +11,52 @@ void solve(stack<int> &inputStack, int count, int size)
         inputStack.pop();
         return;
     }
-    // 13:44
+    int num = inputStack.top();
+    inputStack.pop();
+
+    // recursive call
+    solve(inputStack, count + 1, size);
+
+    inputStack.push(num);
 }
 
 void deletemiddle(stack<int> &inputStack, int N)
 {
-    int cout = 0;
+    int count = 0;
     solve(inputStack, count, N);
 }
 
 int main()
 {
+    stack<int> inputStack;
+
+    // Sample input
+    inputStack.push(1);
+    inputStack.push(2);
+    inputStack.push(3);
+    inputStack.push(4);
+    inputStack.push(5);
+
+    int N = inputStack.size();
+
+    cout << "Stack before deleting middle: ";
+    stack<int> temp = inputStack;
+    while (!temp.empty())
+    {
+        cout << temp.top() << " ";
+        temp.pop();
+    }
+    cout << endl;
+
+    deletemiddle(inputStack, N);
+
+    cout << "Stack after deleting middle: ";
+    while (!inputStack.empty())
+    {
+        cout << inputStack.top() << " ";
+        inputStack.pop();
+    }
+    cout << endl;
+
+    return 0;
 }
